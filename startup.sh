0be6512900f70924935f5c7d6c60e8cd95f4b11d
dbuaon <dubaon@gmail.com>
2024-11-15 16:55:12 -0300

    update startup.sh

--- startup.sh
+++ startup.sh
@@ -20,3 +20,20 @@ echo \
 sudo apt-get update
 
 sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+sudo groupadd docker
+sudo usermod -aG docker $USER  
+
+newgrp docker
+
+sudo systemctl enable docker.service  
+sudo systemctl enable containerd.service  
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+sudo chmod +x /usr/local/bin/docker-compose
+
+docker --version
+docker-compose --version
+
+
